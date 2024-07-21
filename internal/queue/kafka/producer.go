346e3404ba0ac286977e8c3a7a8ebe14174f7c71
Jaewook Lee <wookboy00@naver.com>
2024-07-21 22:31:45 +0900

    Feat: producerPool implemented

---
+++ internal/queue/kafka/producer.go
@@ -0,0 +1,90 @@
+package kafkaqueue
+
+import (
+	"fmt"
+	"github.com/IBM/sarama"
+	"sync"
+)
+
+// ProducerPool is a pool of producers that can be used to produce messages to Kafka for one set of brokers.
+// It is not related to Transaction, Transactional Producer implements by configProvider.
+type ProducerPool struct {
+	locker    sync.Mutex
+	producers []sarama.AsyncProducer
+
+	brokers        []string
+	configProvider func() *sarama.Config
+}
+
+// Take returns a producer from pool. If the producer does not exist, it creates a new one.
+func (p *ProducerPool) Take() (producer sarama.AsyncProducer) {
+	p.locker.Lock()
+	defer p.locker.Unlock()
+
+	if len(p.producers) == 0 {
+		producer = p.generateProducer()
+		return
+	}
+
+	producer = p.producers[0]
+	p.producers = p.producers[1:]
+
+	return
+}
+
+// Return returns a producer to the pool.
+func (p *ProducerPool) Return(producer sarama.AsyncProducer) {
+	p.locker.Lock()
+	defer p.locker.Unlock()
+
+	// If the producer is closed, do not return it to the pool
+	if producer == nil {
+		return
+	}
+
+	// If the producer has an txError, do not return it to the pool
+	if producer.TxnStatus()&sarama.ProducerTxnFlagInError != 0 {
+		_ = producer.Close()
+		return
+	}
+
+	p.producers = append(p.producers, producer)
+}
+
+func (p *ProducerPool) Producers() []sarama.AsyncProducer {
+	return p.producers
+}
+
+func (p *ProducerPool) Close() {
+	p.locker.Lock()
+	defer p.locker.Unlock()
+
+	for _, producer := range p.producers {
+		_ = producer.Close()
+	}
+}
+
+func NewProducerPool(brokers []string, configProvider func() *sarama.Config) *ProducerPool {
+	if configProvider() == nil {
+		panic("configProvider is nil")
+	}
+
+	pool := &ProducerPool{
+		locker:         sync.Mutex{},
+		producers:      []sarama.AsyncProducer{},
+		brokers:        brokers,
+		configProvider: configProvider,
+	}
+
+	return pool
+}
+
+func (p *ProducerPool) generateProducer() sarama.AsyncProducer {
+	producer, err := sarama.NewAsyncProducer(p.brokers, p.configProvider())
+	if err != nil {
+		fmt.Println("Error creating producer", err)
+		return nil
+	}
+
+	return producer
+}
