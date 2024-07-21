7480616f329f41465f4495b18e3665707bdc9c32
Jaewook Lee <wookboy00@naver.com>
2024-07-21 22:31:30 +0900

    Test: kafka consume operator, applied domain operator test

---
+++ internal/queue/kafka/operator_test.go
@@ -0,0 +1,27 @@
+package kafkaqueue
+
+import (
+	"github.com/IBM/sarama"
+	"github.com/violetpay-org/new-queue-manager/queue"
+	"testing"
+)
+
+var brokers = []string{"localhost:9093"}
+
+func mapOperatorProvider(queueName string, serializer queue.MessageSerializer[*sarama.ConsumerMessage, map[string]string], callback queue.Callback[map[string]string]) queue.ConsumeOperator[*sarama.ConsumerMessage, map[string]string] {
+	return NewConsumeOperator(serializer, callback, brokers, queueName, "test-group-id", sarama.NewConfig())
+}
+
+func rawMsgProvider() *sarama.ConsumerMessage {
+	return &sarama.ConsumerMessage{}
+}
+
+func TestConsumeOperator(t *testing.T) {
+	t.Run("map[string]string", func(t *testing.T) {
+		queue.TestSuiteConsumeOperator[*sarama.ConsumerMessage, map[string]string](
+			t,
+			mapOperatorProvider,
+			rawMsgProvider,
+		)
+	})
+}
